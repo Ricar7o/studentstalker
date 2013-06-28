8e8ecdec9228c81b406d2d77b34a56c23252051e
MakerSquare Student <student@lab-3.att.net>
2013-06-28 11:40:01 -0500

    Converted index and show pages to haml

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ActiveRecord::Base
   attr_accessible :address, :age, :email_, :name, :phone_number, :twitter_handle
+  validates :phone_number, :numericality => { :only_integer => true }
+  validates :phone_number, :length => { :is => 10}
 end
