453c7f085a10b5263e9042d743ee28e526ff4fe0
MakerSquare Student <student@lab-3.att.net>
2013-06-28 10:11:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studentstalker::Application.config.secret_token = 'fd76dbe65520ae78a63cc7aa69026810c0ae23854064e91d812dc9338fe0bb047f485afbfb5fe3dee840c2388b6428a9c21f732e9225e1e880687a0b6d19e45d'
